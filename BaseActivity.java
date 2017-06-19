63664401483f448650b17c6be32bb53c29afab58
luoling <782884760@qq.com>
2017-06-19 14:55:03 +0800

    qqqq

--- BaseActivity.java
+++ BaseActivity.java
@@ -12,5 +12,6 @@ public class BaseActivity extends Activity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         int sd =1;
+        int w = 2+1;
     }
 }
